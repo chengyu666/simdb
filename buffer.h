32e4782d0cd2e8ce4a94c2916e89694aad7fa86e
chengyu <fengchengyu123@live.com>
2021-10-25 10:47:07 +0800

    重新规划文件

---
+++ buffer.h
@@ -0,0 +1,24 @@
+class BMgr
+{
+public:
+    BMgr();
+    // Interface functions
+    int FixPage(int page_id, int prot);
+    void NewPage FixNewPage();
+    int UnfixPage(int page_id);
+    int NumFreeFrames();
+    // Internal Functions
+    int SelectVictim();
+    int Hash(int page_id);
+    void RemoveBCB(BCB *ptr, int page_id);
+    void RemoveLRUEle(int frid);
+    void SetDirty(int frame_id);
+    void UnsetDirty(int frame_id);
+    void WriteDirtys();
+    PrintFrame(int frame_id);
+
+private:
+    // Hash Table
+    int ftop[DEFBUFSIZE];
+    BCB *ptof[DEFBUFSIZE];
+};
---
+++ dependencies.h
@@ -0,0 +1,6 @@
+#ifndef DEPEND
+#define DEPEND
+#include <string>
+#define MAXPAGES 50000 //TODO
+using namespace std;
+#endif
